53e90308694d4bea8b3f3d5723434de7f50ac62e
Dmitrii <dbjenov@gmail.com>
2021-05-25 13:13:58 +0300

    new toDo list app

---
+++ src/components/toDoList.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+function ToDoList() {
+   return (
+
+      <ul>
+      <li>
+       <input name="isGoing"type="checkbox"/> Купить молоко
+      </li>
+      <li>
+       <input name="isGoing"type="checkbox"/> Купить хлеб
+      </li>
+      <li>
+       <input name="isGoing"type="checkbox"/> Купить сметану
+      </li>
+    </ul>
+   )
+}
+
+export default ToDoList;
