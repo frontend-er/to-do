b13b19bb98f8edc71fb599ebb39187fdfd72a83b
Dmitrii <dbjenov@gmail.com>
2021-05-28 07:58:46 +0300

    Check box done for every task

--- src/components/Phone/MainContent/Main.jsx
+++ src/components/Phone/MainContent/Main.jsx
@@ -2,16 +2,43 @@
 import React from 'react';
 import style from './Main.module.css'
 import ToDoItem from "./ToDoItem/ToDoItem";
+import todosData from "./../../../todoData";
 
-function Main() {
-   return (
-      <div className={style.mainBody}>
-         <ToDoItem />
-         <ToDoItem />
-         <ToDoItem />
-         <ToDoItem />
-      </div>
-   )
+class Main extends React.Component {
+   constructor () {
+      super()
+      this.state = {
+         todos: todosData
+      }
+      this.handleClick = this.handleClick.bind(this)
+   }
+
+   handleClick (id) {
+      debugger;
+      this.setState(prevState => {
+         const updatedTodos = prevState.todos.map(todo => {
+             if (todo.id === id) {
+                 todo.completed = !todo.completed
+             }
+             return todo
+         })
+         return {
+
+             todos: updatedTodos
+             
+         }
+     })
+   }
+    
+   render () {
+      let todoItems =   this.state.todos.map((i) => { return <ToDoItem handleClick={this.handleClick}  key={i.id} list={i} />});
+      return (
+         <div className={style.mainBody}>
+            {todoItems}
+         </div>
+      )
+   }
+
 }
 
 export default Main;
--- src/components/Phone/MainContent/ToDoItem/ToDoItem.jsx
+++ src/components/Phone/MainContent/ToDoItem/ToDoItem.jsx
@@ -3,10 +3,11 @@ import React from 'react';
 import style from './ToDoItem.module.css'
 
 function ToDoItem(props) {
+   debugger;
    return (
       <div className={style.toDoitem}>
   
-         <input name="isGoing"type="checkbox"/>  Buy a book
+         <input name="isGoing"type="checkbox" checked={props.list.completed} onChange={() => props.handleClick(props.list.id)} />  {props.list.text}
          <br />
       
       </div>
